630d4673300bffbb21dcf9422542cd7251499609
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-04-26 10:10:11 -0500

    Almost Done with Scanner Class

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,13 +3,23 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
-        System.out.printf("The value of pi is approximately %s.", pi ); //can't figure out the format part
+        System.out.format("The value of pi is approximately: %.2f%n", pi );
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter an integer: ");
         int userNumber = sc.nextInt();
         System.out.printf("Thank you for entering an integer of %d.", userNumber);
 
+        sc.nextLine();
+        System.out.println("Please enter 3 words: ");
+        String wordOne = sc.nextLine();
+        String wordTwo = sc.nextLine();
+        String wordThree = sc.nextLine();
+        System.out.printf("You entered these 3 words: %s,%s, %s\n",wordOne,wordTwo,wordThree);
+
+        System.out.println("Please type a sentence: ");
+        String userSentence = sc.nextLine();
+        System.out.printf("The sentence you wrote was: %s\n", userSentence);
 
     }
 }
