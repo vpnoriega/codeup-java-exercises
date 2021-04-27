29227dc86830d5ed3800a8ce519285ae630b331e
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-04-27 12:37:27 -0500

    Started Exercise

---
+++ src/MethodsExercises.java
@@ -0,0 +1,28 @@
+public class MethodsExercises {
+    public static void main(String[] arr) {
+        System.out.println(Addition(1, 2));
+        System.out.println(Subtraction(3, 2));
+//        System.out.println(Multiplication(3,2));
+//        System.out.println(Division(8,4));
+
+
+    }
+
+    public static int Addition(int num1, int num2) {
+        return num1 + num2;
+
+    }
+
+    public static int Subtraction(int num1, int num2) {
+        return num1 - num2;
+
+    }
+
+//    public static double Multiplication(double num1, double num2) {
+//
+//    }
+//
+//    public static double Division(double num1, double num2) {
+//
+//    }
+}
