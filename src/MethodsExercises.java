ed3ea4540b8dc83622f1ad09c39469b57f122112
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-04-28 10:32:21 -0500

    Break Push

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,5 +1,6 @@
 import org.w3c.dom.ls.LSOutput;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 public class MethodsExercises {
     // 1.)
@@ -9,7 +10,7 @@ public class MethodsExercises {
         System.out.println(Multiplication(3,2));
         System.out.println(Division(8,2));
         System.out.println(Modulus(9,3));
-        getInteger(1,10);
+        //getInteger(1,10);
 
 
     }
@@ -43,27 +44,87 @@ public class MethodsExercises {
     // IF it's INVALID, prompt the user again
     // but if it's VALID? What do we do, Exit the Loop?
 
-    public static void getInteger(int min, int max) {
-        Scanner sc = new Scanner(System.in);
-        boolean wrongInput;
+//    public static void getInteger(int min, int max) {
+//        Scanner sc = new Scanner(System.in);
+//        boolean wrongInput;
+//
+//        do {
+//            System.out.printf("Enter a number between %d and %d", min, max);
+//            int userNumber = Integer.parseInt(sc.next());
+//
+//            if(userNumber >= min && userNumber <= max){
+//                wrongInput = false; //exits loop, because the user's input was valid
+//            } else{
+//                System.out.println("Please Enter a number again: ");
+//                wrongInput = true; //loop again
+//            }
+//
+//        } while (wrongInput);
+//        System.out.println("Thank you for entering a number within the range!");
+//
+//    }
 
-        do {
-            System.out.printf("Enter a number between %d and %d", min, max);
-            int userNumber = Integer.parseInt(sc.next());
+    //3. Calculate the factorial of a number.
 
-            if(userNumber >= min && userNumber <= max){
-                wrongInput = false; //exits loop
-            } else{
-                System.out.println("Please Enter a number again: ");
-                wrongInput = true; //loop again
-            }
+    //Prompt the user // sout/scanner
+    //Display the factorial of the userInput //find a way to divide
+    //Ask if the user wants to continue // confirmation (loop)
+    //Use a loop to calculate the factorial //why do we have to use a loop?
+    //verify that the number is 1-10 // why are we checking if the number is 1-10 AFTER calculating the factorial
 
-        } while (wrongInput);
-        System.out.println("Thank you for entering a number within the range!");
+    // use long to STORE to the factorial // need a variable to store it
+    // CONTINUE if the user agrees //confirmation
 
-    }
+    // Factorials are denoted by the !
 
-    //3.
+
+//    public static long getFactorial(int num) {
+//        Scanner sc = new Scanner(System.in);
+//        //boolean wrongInput;
+//        boolean confirmation;
+//
+//
+//        do {
+//            System.out.printf("Enter a number between 1-10");
+//            int userNumber = Integer.parseInt(sc.next());
+//            System.out.println(userNumber);
+//
+//            System.out.println("Would you link to continue?[y/n]");
+//            String userInput = sc.next();
+//
+//            //??How do we get the user's number to display the factors of itself
+//
+//            if(confirmation = userInput.equalsIgnoreCase("y"));
+//
+//            {
+//                long fact = 1;
+//                for (int i = 2; i <= num; i++) {
+//                    fact = fact * i;
+//                }
+//                return fact;
+//            }
+//
+////            if(userNumber >= min && userNumber <= max){
+////                wrongInput = false; //exits loop, because the user's input was valid
+////            } else{
+////                System.out.println("Please Enter a number again: ");
+////                wrongInput = true; //loop again
+////            }
+//
+//
+//
+//        }while((confirmation));
+//    }
+
+    //4. Create an application that simulates dice rolling:
+public static void diceRoll() {
+    int diceOne = (int) (Math.random() * 6 - 1 + 1) + 1;
+    int diceTwo = (int) (Math.random() * 6 - 1 + 1) + 1;
+    System.out.println("Enter the number of Sides for a pair of dice: ");
+}
+
+
+
 
 
 }
