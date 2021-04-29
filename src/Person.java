7d912d390497c84d2013920814e17bf8bc9f8da4
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-04-29 17:19:01 -0500

    Started Person Exercise

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Person {
+public class Person {
+    private String name;
+
+    public String getName(){
+//TODO: return the person's name
+        return name;
+    }
+
+    public void setName(String name){
+//TODO: change the name property to the passed value
+        this.name = name;
+    }
+    public void sayHello(){
+//TODO: print a message to the console using the person's name
+        System.out.println("Hello from " + this.name);
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+
+        Person person1 = new Person("Vanessa");
+        System.out.println(person1.getName());
+
+    }
+
+
 }
