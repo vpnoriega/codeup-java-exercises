317eed2354cad5c40c60935abc11e91f8dab6ab8
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-04-28 20:59:17 -0500

    Finished Exercise

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,5 +1,9 @@
 public class StringExercise {
     public static void main(String [] args){
+        String sentence = "We don't need no education\n" +
+                "We don't need no thought control";
+        System.out.println(sentence + "\nCheck \"this\" out!, \"s inside of \"s!" + "\nIn windows, the main drive is usually C:\\" + "\nI can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!");
 
     }
 }
