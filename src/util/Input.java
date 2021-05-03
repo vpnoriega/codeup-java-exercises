18e7804e300b56ee79a6ae77237e97a33d237f98
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-05-02 23:25:43 -0500

    Fixed a minor error

--- src/util/Input.java
+++ src/util/Input.java
@@ -3,43 +3,42 @@ package util;
 import java.util.Scanner;
 
 public class Input {
-    private class scanner{
+    private class scanner {
         Scanner sc = new Scanner(System.in);
 
-        public String getString(){
+        public String getString() {
             return this.sc.nextLine();
         }
 
         public boolean yesNo() {
             String input = sc.nextLine();
             return (input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes"));
-    }
+        }
 
-        public int getInt(int min, int max){
+        public int getInt(int min, int max) {
             System.out.println("Enter a number between " + min + " and " + max + ".");
             int num = sc.nextInt();
 
-            if (num >= min && num <= max){
+            if (num >= min && num <= max) {
                 return num;
-            }
-            else {
+            } else {
                 System.out.println("Your number is invalid.");
                 return getInt(min, max);
             }
         }
 
-        public double getDouble(double min, double max){
+        public double getDouble(double min, double max) {
             System.out.println("Enter a decimal number between " + min + " and " + max + ".");
             double num = sc.nextInt();
 
-            if (num >= min && num <= max){
+            if (num >= min && num <= max) {
                 return num;
-            }
-            else {
+            } else {
                 System.out.println("Your number is invalid.");
                 return getDouble(min, max);
             }
         }
 
 
+    }
 }
