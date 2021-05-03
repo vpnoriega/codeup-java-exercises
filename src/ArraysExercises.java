4f8f2d760c7a15f095107cdf82507a0458b07e65
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-05-03 17:03:15 -0500

    first attempt at exercise

---
+++ src/ArraysExercises.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class ArraysExercises {
+    public static void main(String[] args){
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
+
+
+        String[] person = new String [3];
+        System.out.println(Arrays.toString(person));
+
+        Person person1 = new Person("Vanessa");
+        Person person2 = new Person("Sergio");
+        Person person3 = new Person("Iffy");
+
+        System.out.println(Arrays.toString(person[0]));
+
+
+
+
+
+
+
+    }
+}
