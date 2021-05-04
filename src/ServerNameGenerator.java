8ad3fc43a17c35e40977bdf1288a2fedd94d7c0f
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-05-03 22:40:05 -0500

    Started but incomplete

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,22 @@
+public class ServerNameGenerator {
+
+    public static void main(String[] args){
+
+        /** Create two arrays whose elements are strings: one with at least 10 adjectives, another with at least 10 nouns: */
+
+        String [] adjectives = {"pretty", "ugly", "tall","short","thin","thick","happy","sad","excited", "relaxed"};
+
+        String [] nouns = {"library", "school", "laptop","monitor","notebook","pencil","keyboard", "mouse", "headphones", "Codey the duck"};
+
+        System.out.println("Here is your server name: ");
+        System.out.println(); //enter server name here
+
+    }
+
+    /** Create a method that will return a random element from an array of strings: */
+//?? random element? How??
+
+//    public static String randomElement(String[] name){
+//
+//    }
+}
