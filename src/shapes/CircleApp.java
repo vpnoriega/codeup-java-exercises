8949c272a56152ce956804c2d98fdc9c525ef64e
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-05-04 15:13:32 -0500

    adding files

---
+++ src/shapes/CircleApp.java
@@ -0,0 +1,12 @@
+package shapes;
+
+import util.Input;
+
+import java.util.Scanner;
+
+public class CircleApp {
+
+    //prompt the user for radius using Input class
+    // create a circle object
+    //display the circumference and area
+}
