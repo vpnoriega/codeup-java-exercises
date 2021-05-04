1c7f86107982589c8b01bc80230ef8ffd393012d
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-05-03 22:21:26 -0500

    Need to go back for the last part

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -6,14 +6,30 @@ public class ArraysExercises {
         System.out.println(Arrays.toString(numbers));
 
 
-        String[] person = new String [3];
-        System.out.println(Arrays.toString(person));
+        //String[] person = new String [3];
+        //System.out.println(Arrays.toString(person)); //not sure what this does?
+
+        /** Create an array that holds 3 Person objects. Assign a new instance of the Person class to each element. Iterate through the array and print out the name of each person in the array.*/
 
         Person person1 = new Person("Vanessa");
         Person person2 = new Person("Sergio");
         Person person3 = new Person("Iffy");
 
-        System.out.println(Arrays.toString(person[0]));
+        String[] people = {person1.getName(), person2.getName(), person3.getName()};
+        System.out.println(Arrays.toString(people)); //this prints the array of names
+
+        for (String person : people) {
+            System.out.println(person); //this prints the names on separate lines
+        }
+
+        /** Create a static method named addPerson.
+         * Should accept an array of Person objects, as well as a single person object to add to the passed array
+         * Should return an array whose length is 1 greater than the passed array, with the passed person object at the end of the array. */
+
+//        public static void addPerson(String[] people, String person){
+//
+//
+//        }
 
 
 
