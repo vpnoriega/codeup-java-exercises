6b719e4d4b47f0221e1a6b0e41f90f48f83b85e0
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-05-04 22:08:16 -0500

    Rectangle and Square Set up

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,4 +1,21 @@
 package shapes;
 
 public class Rectangle {
+    protected double length;
+    protected double width;
+
+    public Rectangle(int length, int width){
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getArea(){
+        return length * width;
+
+    }
+
+    public double getPerimeter(){
+        return (length * 2) + (width * 2);
+    }
+
 }
