0d0866bb730cecf3d4b7c98a8785094da9f24c23
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-05-04 22:07:02 -0500

    Bus and Vehicle Lecture

--- src/InheritanceAndPolymorphism/Vehicle.java
+++ src/InheritanceAndPolymorphism/Vehicle.java
@@ -2,21 +2,27 @@ package InheritanceAndPolymorphism;
 public class Vehicle {
     private int numberOfOccupants;
     private int maxSpeed;
+
     public Vehicle() {
         this.maxSpeed = 0;
     }
+
     public Vehicle(int maxSpeed) {
         this.maxSpeed = maxSpeed;
     }
+
     public void start() {
         System.out.println("Starting... ");
     }
+
     public int getNumberOfOccupants() {
         return this.numberOfOccupants;
     }
+
     public int getMaxSpeed() {
         return maxSpeed;
     }
+
     public void setNumberOfOccupants(int numberOfOccupants) {
         this.numberOfOccupants = numberOfOccupants;
     }
