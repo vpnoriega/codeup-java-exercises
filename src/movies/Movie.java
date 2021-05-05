fc0205b3b168b1cdabfd718d83e99e9b919cf281
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-05-05 08:41:59 -0500

    In Progress: Movies Exercise

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -1,2 +1,27 @@
-package movies;public class Movie {
+package movies;
+
+/** Create a class named Movie.*/
+public class Movie {
+
+    /** Should have private fields for name and category: */
+    private String name;
+    private String category;
+
+/** and a constructor that sets both of these: */
+    public Movie(String name, String category){
+        this.name = name;
+        this.category = category;
+
+    }
+
+/** Create methods to access these properties and change them (getters and setters).*/
+    public void setMovie(String name, String category){
+        this.name = name;
+        this.category = category;
+    }
+
+    public String[] getMovie(){
+        String[] movie = {this.name, this.category};
+        return movie;
+    }
 }
--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,2 +1,9 @@
-package movies;public class MoviesApplication {
+package movies;
+
+/** Create a class named MoviesApplication that has a main method: */
+public class MoviesApplication {
+    public static void main(String[] args) {
+
+        
+    }
 }
