864bc3ed6bcdd7e372781e9420edd2848e6b5d87
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-05-05 12:37:42 -0500

    completed: Circle and CircleApp

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,12 +1,21 @@
 package shapes;
-
 import util.Input;
-
 import java.util.Scanner;
 
 public class CircleApp {
+    public static void main(String [] args) {
 
-    //prompt the user for radius using Input class
-    // create a circle object
-    //display the circumference and area
+        //prompt the user for radius using Input class
+        double userInput;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the radius: ");
+        userInput = sc.nextDouble();
+
+        // create a circle object
+        Circle circle = new Circle(userInput);
+
+        //display the circumference and area
+        System.out.printf("Your circle's area is: %.2f\n", circle.getArea());
+        System.out.printf("Your circle's circumference: %.2f", circle.getCircumference());
+    }
 }
