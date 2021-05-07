71b039e1e7882b1cf576559d95c092ecd8eaeabb
vanessa noriega <vanessa.p.noriega@gmail.com>
2021-05-07 17:01:25 -0500

    In progress: GradesApp Exercise

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,39 +1,60 @@
 package grades;
-
+import util.Input;
 import java.util.*;
+import java.util.logging.SocketHandler;
 
 public class GradesApplication {
 
-    public static void main(String[] args){
+    private static Input userInput = new Input();
+    private static HashMap<String, Student> students;
 
-        HashMap<String, String> Students = new HashMap<>();
-        Student Vanessa = new Student("Vanessa");
-        Vanessa.addGrade(85);
-        Vanessa.addGrade(90);
-        Vanessa.addGrade(88);
-
-        Student Thoms = new Student("Thoms");
-        Thoms.addGrade(93);
-        Thoms.addGrade(95);
-        Thoms.addGrade(91);
-
-        Student Roland = new Student("Roland");
-        Roland.addGrade(96);
-        Roland.addGrade(93);
-        Roland.addGrade(90);
-
-        Student Shanshan = new Student("Shanshan");
-        Shanshan.addGrade(100);
-        Shanshan.addGrade(99);
-        Shanshan.addGrade(98);
-
-        Students.put("Vanessa", "vpnoriega");
-        Students.put("Thoms", "ThomsAlexander23");
-        Students.put("Roland", "roland-valdez");
-        Students.put("Shanshan","shanshan-su");
-
-        System.out.println(Students);
+//    Scanner sc = new Scanner(System.in);
+//    boolean match;
 
 
+    public static void main(String[] args) {
+        System.out.println("Welcome!\nHere are the GitHub usernames of our students: ");
+        students = getStudents();
     }
+
+    protected static void showStudents(HashMap<String, Student> Students){
+        for (String student : students.keySet()){
+            System.out.println(" | " + student);
+        }
+    }
+
+        protected static HashMap<String, Student> getStudents(){
+            HashMap<String, Student> Students = new HashMap<>();
+
+            Student Vanessa = new Student("Vanessa");
+            Vanessa.addGrade(85);
+            Vanessa.addGrade(90);
+            Vanessa.addGrade(88);
+
+            Student Thoms = new Student("Thoms");
+            Thoms.addGrade(93);
+            Thoms.addGrade(95);
+            Thoms.addGrade(91);
+
+            Student Roland = new Student("Roland");
+            Roland.addGrade(96);
+            Roland.addGrade(93);
+            Roland.addGrade(90);
+
+            Student Shanshan = new Student("Shanshan");
+            Shanshan.addGrade(100);
+            Shanshan.addGrade(99);
+            Shanshan.addGrade(98);
+
+            Students.put("vpnoriega", Vanessa);
+            Students.put("ThomsAlexander23", Thoms);
+            Students.put("roland-valdez", Roland);
+            Students.put("shanshan-su", Shanshan);
+
+            return Students;
+
+        }
+
 }
+
+
