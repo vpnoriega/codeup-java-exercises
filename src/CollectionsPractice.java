38479e37efa20555bea6e28a62b4f14fa4b070e9
vanessa noriega <vanessa.p.noriega@gmail.com>
2021-05-07 12:22:22 -0500

    fixed the printing arrays issue

--- src/CollectionsPractice.java
+++ src/CollectionsPractice.java
@@ -1,21 +1,17 @@
 import java.util.*;
 
 public class CollectionsPractice {
-    public static void main(String[] args) {
 
-        ArrayList<String> fruit = new ArrayList<>();
-        fruit.add("banana");
-        fruit.add("apple");
-        fruit.add("strawberries");
-        System.out.println(fruit);
+        //ArrayList<String> fruit = new ArrayList<>(Arrays.asList("banana","apple","strawberries"));
+
+    public static ArrayList<String> upperLastElement(ArrayList<String> fruit) {
+        return fruit;
 
     }
 
-    public static void upperLastElement(ArrayList<String> strings) {
-        for (String str : strings) {
-            System.out.println(str);
-        }
+        public static void main(String[] args) {
+            ArrayList<String> fruit = new ArrayList<>(Arrays.asList("banana","apple","strawberries"));
 
-
+            System.out.println("The fruit listed is " + fruit);
     }
 }
